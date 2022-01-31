845a6725aad85fd071d6ba29d878a1592e9a4c82
Maurendra <mrendra25@gmail.com>
2022-01-31 09:07:40 +0700

    commit

--- src/config/data/Provider.js
+++ src/config/data/Provider.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API_BASE_URL = "http://localhost:3001";
+// const API_BASE_URL = "http://localhost:3001";
+const API_BASE_URL = "https://mhc-backend.herokuapp.com";
 
 const Provider = {
   _axios: null,
